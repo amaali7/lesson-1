9bacd318107191cc7508ce73e6ba6770f09726ae
Abdallah Ali <amaali1991@gmail.com>
2020-05-16 13:26:09 +0200

    finsh 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
+
+import HomePage from './pages/homepage/homepage.component'
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return <HomePage/>;
   }
 }
 
